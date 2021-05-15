a824dc86fde14920faac4f8cd1d9f9a8024c6ad8
Kaiser Quezada <kaiser@sci5.com>
2021-05-15 23:30:36 +0800

    added routing, withrouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     <div
-        style={{
-
-        }}
         className={`${size} menu-item`}
+        onClick={()=> history.push(`${match.url}${linkUrl}`)}
     >
         <div className='background-image'
         style={{
@@ -20,4 +18,4 @@ const MenuItem = ({title, imageUrl, size}) => (
     </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
